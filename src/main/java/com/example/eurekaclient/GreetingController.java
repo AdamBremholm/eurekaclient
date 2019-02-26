f4bd1aca3657e5330e0b662c3200a7f605de1551
Martin Blomberg <martin.blomberg@outlook.com>
2019-02-26 13:56:38 +0100

    Added route for returning all registered clients

--- src/main/java/com/example/eurekaclient/GreetingController.java
+++ src/main/java/com/example/eurekaclient/GreetingController.java
@@ -1,9 +1,14 @@
 package com.example.eurekaclient;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class GreetingController {
@@ -23,6 +28,16 @@ public class GreetingController {
     public String randomHello() {
 
         return restTemplate.getForObject("http://greeting-app/hello", String.class);
+    }
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @GetMapping("/clients")
+    public List<InstanceInfo> clients(){
+
+        Application application = eurekaClient.getApplication("greeting-app");
+        return application.getInstances();
     }
 
 }
